0795908e8f643c5835bfafab68ead3aa50c27766
kanastasoff <kireprogramista@gmail.com>
2020-09-26 17:11:15 +0300

    crud services

--- http-app/src/App.js
+++ http-app/src/App.js
@@ -2,26 +2,44 @@ import React, { Component } from "react";
 import axios from 'axios';
 import "./App.css";
 
+const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+
 class App extends Component {
   state = {
     posts: []
   };
   
   async componentDidMount() {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const { data: posts } = await axios.get(apiEndpoint);
     this.setState({posts});
 
   }
 
-  handleAdd = () => {
-    console.log("Add");
+  handleAdd = async () => {
+    const obj = {title: 'a', body: 'b'};
+    const {data: post } = await axios.post(apiEndpoint, obj);
+    console.log(post);
+    const posts = [post, ...this.state.posts];
+    this.setState({ posts });
+
   };
 
-  handleUpdate = post => {
-    console.log("Update", post);
+  handleUpdate = async post => {
+    post.title="Updated";
+    await axios.put(apiEndpoint + '/' + post.id, post);
+
+    const posts =[...this.state.posts];
+    const index = posts.indexOf(post);
+    posts[index] = {...post};
+    this.setState({posts});
   };
 
-  handleDelete = post => {
+  handleDelete = async post => {
+    await axios.delete(apiEndpoint + '/' + post.id);
+
+    const posts = this.state.posts.filter(p=>p.id !==post.id);
+    this.setState({ posts});
+
     console.log("Delete", post);
   };
 
